b6ba4fa649442ab36bcc09dcfe8679e19ce616aa
junbo.dai <bobodai515@163.com>
2026-04-12 16:26:10 +0800

    feat: completed 06

--- exercises/06_stack_maze/06_stack_maze.c
+++ exercises/06_stack_maze/06_stack_maze.c
@@ -11,10 +11,61 @@ int maze[MAX_ROW][MAX_COL] = {
 	0, 0, 0, 1, 0,
 };
 
-int main(void)
-{
-	// TODO: 在这里添加你的代码
-    // I AM NOT DONE
+typedef struct {
+    int row;
+    int col;
+} Position;
 
-	return 0;
+int visited[MAX_ROW][MAX_COL] = {0};
+int prev_row[MAX_ROW][MAX_COL] = {0};
+int prev_col[MAX_ROW][MAX_COL] = {0};
+
+Position stack[MAX_ROW * MAX_COL];
+int top = -1;
+
+void push(Position p) { stack[++top] = p; }
+Position pop(void) { return stack[top--]; }
+int is_empty(void) { return top == -1; }
+
+int main(void) {
+    Position start = {0, 0};
+    Position end = {MAX_ROW - 1, MAX_COL - 1};
+
+    push(start);
+    visited[0][0] = 1;
+    prev_row[0][0] = -1;
+    prev_col[0][0] = -1;
+
+    int directions[4][2] = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
+
+    while (!is_empty()) {
+        Position cur = pop();
+
+        if (cur.row == end.row && cur.col == end.col) {
+            Position path[MAX_ROW * MAX_COL];
+            int count = 0;
+            Position p = end;
+            while (p.row != -1) {
+                path[count++] = p;
+                p = (Position){prev_row[p.row][p.col], prev_col[p.row][p.col]};
+            }
+            for (int i = 0; i < count; i++) {
+                printf("(%d, %d)\n", path[i].row, path[i].col);
+            }
+            return 0;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nr = cur.row + directions[i][0];
+            int nc = cur.col + directions[i][1];
+            if (nr >= 0 && nr < MAX_ROW && nc >= 0 && nc < MAX_COL
+                && maze[nr][nc] == 0 && !visited[nr][nc]) {
+                visited[nr][nc] = 1;
+                prev_row[nr][nc] = cur.row;
+                prev_col[nr][nc] = cur.col;
+                push((Position){nr, nc});
+            }
+        }
+    }
+    return 0;
 }
