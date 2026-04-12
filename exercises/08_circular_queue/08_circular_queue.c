8113cbca2ff10c6de033b638a1a3843990ff1599
junbo.dai <bobodai515@163.com>
2026-04-12 16:44:27 +0800

    feat: completed 08

--- exercises/08_circular_queue/08_circular_queue.c
+++ exercises/08_circular_queue/08_circular_queue.c
@@ -14,14 +14,58 @@ typedef struct {
     int count;
 } Queue;
 
+void init(Queue *q) {
+    q->head = 0;
+    q->tail = 0;
+    q->count = 0;
+}
+
+bool is_empty(Queue *q) {
+    return q->count == 0;
+}
+
+bool is_full(Queue *q) {
+    return q->count == MAX_PEOPLE;
+}
+
+void enqueue(Queue *q, People p) {
+    if (!is_full(q)) {
+        q->data[q->tail] = p;
+        q->tail = (q->tail + 1) % MAX_PEOPLE;
+        q->count++;
+    }
+}
+
+People dequeue(Queue *q) {
+    People p = q->data[q->head];
+    q->head = (q->head + 1) % MAX_PEOPLE;
+    q->count--;
+    return p;
+}
+
 int main() {
     Queue q;
-    int total_people=50;
-    int report_interval=5;
+    int total_people = 50;
+    int report_interval = 5;
 
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
-    
+    init(&q);
+
+    for (int i = 1; i <= total_people; i++) {
+        People p = {i};
+        enqueue(&q, p);
+    }
+
+    int count = 1;
+    while (q.count > 1) {
+        People p = dequeue(&q);
+        if (count % report_interval == 0) {
+            printf("淘汰: %d\n", p.id);
+        } else {
+            enqueue(&q, p);
+        }
+        count++;
+    }
+
     printf("最后剩下的人是: %d\n", q.data[q.head].id);
 
     return 0;
