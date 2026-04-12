dd281ec4c4e39a684c805838f8724cb8f888c938
junbo.dai <bobodai515@163.com>
2026-04-12 16:41:48 +0800

    feat: completed 07

--- exercises/07_queue_maze/07_queue_maze.c
+++ exercises/07_queue_maze/07_queue_maze.c
@@ -3,18 +3,69 @@
 #define MAX_ROW 5
 #define MAX_COL 5
 
+typedef struct {
+    int row;
+    int col;
+} Position;
+
 int maze[MAX_ROW][MAX_COL] = {
-	0, 1, 0, 0, 0,
-	0, 1, 0, 1, 0,
-	0, 0, 0, 0, 0,
-	0, 1, 1, 1, 0,
-	0, 0, 0, 1, 0,
+    0, 1, 0, 0, 0,
+    0, 1, 0, 1, 0,
+    0, 0, 0, 0, 0,
+    0, 1, 1, 1, 0,
+    0, 0, 0, 1, 0,
 };
 
-int main(void)
-{
-	// TODO: 在这里添加你的代码
-    // I AM NOT DONE
+int visited[MAX_ROW][MAX_COL] = {0};
+int prev_row[MAX_ROW][MAX_COL] = {0};
+int prev_col[MAX_ROW][MAX_COL] = {0};
 
-	return 0;
+Position queue[MAX_ROW * MAX_COL];
+int front = 0, rear = 0;
+
+void enqueue(Position p) { queue[rear++] = p; }
+Position dequeue(void) { return queue[front++]; }
+int is_empty(void) { return front == rear; }
+
+int main(void) {
+    Position start = {0, 0};
+    Position end = {MAX_ROW - 1, MAX_COL - 1};
+
+    enqueue(start);
+    visited[0][0] = 1;
+    prev_row[0][0] = -1;
+    prev_col[0][0] = -1;
+
+    int directions[4][2] = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
+
+    while (!is_empty()) {
+        Position cur = dequeue();
+
+        if (cur.row == end.row && cur.col == end.col) {
+            Position path[MAX_ROW * MAX_COL];
+            int count = 0;
+            Position p = end;
+            while (p.row != -1) {
+                path[count++] = p;
+                p = (Position){prev_row[p.row][p.col], prev_col[p.row][p.col]};
+            }
+            for (int i = 0; i < count; i++) {
+                printf("(%d, %d)\n", path[i].row, path[i].col);
+            }
+            return 0;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nr = cur.row + directions[i][0];
+            int nc = cur.col + directions[i][1];
+            if (nr >= 0 && nr < MAX_ROW && nc >= 0 && nc < MAX_COL
+                && maze[nr][nc] == 0 && !visited[nr][nc]) {
+                visited[nr][nc] = 1;
+                prev_row[nr][nc] = cur.row;
+                prev_col[nr][nc] = cur.col;
+                enqueue((Position){nr, nc});
+            }
+        }
+    }
+    return 0;
 }
